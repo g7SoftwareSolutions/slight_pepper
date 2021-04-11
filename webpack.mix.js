8562017a76e4368de8c739315ee66f63e020ffdc
g7SoftwareSolutions <76829249+g7SoftwareSolutions@users.noreply.github.com>
2021-04-11 19:49:23 -0400

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,22 @@
 const path = require('path')
-// const fs = require('fs-extra')
+const fs = require('fs-extra')
 const mix = require('laravel-mix')
-// require('laravel-mix-versionhash')
+require('laravel-mix-versionhash')
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 mix
-  .js('resources/js/app.js', 'public/dist/js/')
+  .js('resources/js/app.js', 'public/dist/js')
   .sass('resources/sass/app.scss', 'public/dist/css')
 
-  // .disableNotifications()
+  .disableNotifications()
 
 if (mix.inProduction()) {
   mix
     // .extract() // Disabled until resolved: https://github.com/JeffreyWay/laravel-mix/issues/1889
     // .version() // Use `laravel-mix-versionhash` for the generating correct Laravel Mix manifest file.
-    // .versionHash()
+    .versionHash()
 } else {
-  // mix.sourceMaps()
+  mix.sourceMaps()
 }
 
 mix.webpackConfig({
@@ -29,24 +29,24 @@ mix.webpackConfig({
       '~': path.join(__dirname, './resources/js')
     }
   },
-  // output: {
-  //   chunkFilename: 'dist/js/app.js',
-  //   path: mix.config.hmr
-  //     ? '/'
-  //     : path.resolve(__dirname, mix.inProduction() ? './public/build' : './public')
-  // }
+  output: {
+    chunkFilename: 'dist/js/[chunkhash].js',
+    path: mix.config.hmr
+      ? '/'
+      : path.resolve(__dirname, mix.inProduction() ? './public/build' : './public')
+  }
 })
 
-// mix.then(() => {
-//   if (mix.inProduction()) {
-//     process.nextTick(() => publishAseets())
-//   }
-// })
+mix.then(() => {
+  if (mix.inProduction()) {
+    process.nextTick(() => publishAseets())
+  }
+})
 
 function publishAseets () {
   const publicDir = path.resolve(__dirname, './public')
 
-  // fs.removeSync(path.join(publicDir, 'dist'))
-  // fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'))
-  // fs.removeSync(path.join(publicDir, 'build'))
+  fs.removeSync(path.join(publicDir, 'dist'))
+  fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'))
+  fs.removeSync(path.join(publicDir, 'build'))
 }
